96e8ebe5b502da60e039d3a8c655dc061919e5d5
Michael Brooks <michael@michaelbrooks.ca>
2012-10-05 17:04:05 -0700

    Add spec for local and remote phonegap.env

--- spec/phonegap.spec.js
+++ spec/phonegap.spec.js
@@ -9,5 +9,13 @@ describe('phonegap', function() {
         it('should default to local machine', function() {
             expect(phonegap.env()).toEqual('local');
         });
+
+        it('should support local environment', function() {
+            expect(phonegap.env('local')).toEqual('local');
+        });
+
+        it('should support remote environment', function() {
+            expect(phonegap.env('remote')).toEqual('remote');
+        });
     });
 });
