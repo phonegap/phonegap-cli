8548a8f89b24112d6a91dda727e7578f13adb400
Michael Brooks <michael@michaelbrooks.ca>
2012-10-10 15:47:32 -0700

    Stub phonegap-build interface.

--- spec/phonegap-build.spec.js
+++ spec/phonegap-build.spec.js
@@ -1,15 +1,69 @@
 describe('phonegap-build', function() {
     var build = require('../lib/phonegap-build');
 
-    describe('create command', function() {
+    describe('create', function() {
         it('should exist', function() {
             expect(build.create).toBeDefined();
         });
     });
 
-    describe('platform command', function() {
-        it('should exist', function() {
-            expect(build.platform).toBeDefined();
+    describe('user', function() {
+        describe('login', function() {
+            it('should exist', function() {
+                expect(build.user.login).toBeDefined();
+            });
+        });
+
+        describe('logout', function() {
+            it('should exist', function() {
+                expect(build.user.logout).toBeDefined();
+            });
+        });
+    });
+
+    describe('app', function() {
+        describe('create', function() {
+            it('should exist', function() {
+                expect(build.app.create).toBeDefined();
+            });
+        });
+
+        describe('destroy', function() {
+            it('should exist', function() {
+                expect(build.app.destroy).toBeDefined();
+            });
+        });
+
+        describe('list', function() {
+            it('should exist', function() {
+                expect(build.app.list).toBeDefined();
+            });
+        });
+
+        describe('info', function() {
+            it('should exist', function() {
+                expect(build.app.info).toBeDefined();
+            });
+        });
+    });
+
+    describe('platform', function() {
+        describe('add', function() {
+            it('should exist', function() {
+                expect(build.platform.add).toBeDefined();
+            });
+        });
+
+        describe('remove', function() {
+            it('should exist', function() {
+                expect(build.platform.remove).toBeDefined();
+            });
+        });
+
+        describe('list', function() {
+            it('should exist', function() {
+                expect(build.platform.list).toBeDefined();
+            });
         });
     });
 });
