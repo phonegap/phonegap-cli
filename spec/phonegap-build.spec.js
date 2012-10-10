411c3a7820097f3de0cfac7bd1e9303ade422e48
Michael Brooks <michael@michaelbrooks.ca>
2012-10-10 15:29:58 -0700

    Add phonegap-build platform command.

--- spec/phonegap-build.spec.js
+++ spec/phonegap-build.spec.js
@@ -1,9 +1,15 @@
 describe('phonegap-build', function() {
-    var phonegapBuild = require('../lib/phonegap-build');
+    var build = require('../lib/phonegap-build');
 
     describe('create command', function() {
         it('should exist', function() {
-            expect(phonegapBuild.create).toBeDefined();
+            expect(build.create).toBeDefined();
+        });
+    });
+
+    describe('platform command', function() {
+        it('should exist', function() {
+            expect(build.platform).toBeDefined();
         });
     });
 });
