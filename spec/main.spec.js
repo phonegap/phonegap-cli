bd3329441887bfe157dc156c1bdde6b6d6620838
Michael Brooks <michael@michaelbrooks.ca>
2013-03-23 18:04:45 -0700

    [npm] Change main entry to an instance of PhoneGap.

---
+++ spec/main.spec.js
@@ -0,0 +1,16 @@
+/*!
+ * Module dependencies.
+ */
+
+var PhoneGap = require('../lib/phonegap'),
+    phonegap = require('../lib/main');
+
+/*!
+ * Specification: phonegap
+ */
+
+describe('main', function() {
+    it('should be an instance of PhoneGap', function() {
+        expect(phonegap).toEqual(jasmine.any(PhoneGap));
+    });
+});
