46338227fe7818949703ee1c7ef0c9889d8ad63c
Michael Brooks <michael@michaelbrooks.ca>
2013-03-31 18:41:05 -0700

    [spec] Update phonegap spec coverage.

--- spec/phonegap.spec.js
+++ spec/phonegap.spec.js
@@ -10,23 +10,39 @@ var PhoneGap = require('../lib/phonegap'),
  */
 
 describe('phonegap', function() {
-    it('should have an app function', function() {
+    it('should define phonegap.app', function() {
         expect(phonegap.app).toEqual(jasmine.any(Function));
     });
 
-    it('should have a create function', function() {
-        expect(phonegap.create).toEqual(jasmine.any(Function));
-    });
-
-    it('should have a build function', function() {
+    it('should define phonegap.build', function() {
         expect(phonegap.build).toEqual(jasmine.any(Function));
     });
 
-    it('should have a local object', function() {
+    it('should define phonegap.create', function() {
+        expect(phonegap.create).toEqual(jasmine.any(Function));
+    });
+
+    it('should define phonegap.local', function() {
         expect(phonegap.local).toEqual(jasmine.any(Object));
     });
 
-    it('should have a remote function', function() {
+    it('should define phonegap.local.build', function() {
+        expect(phonegap.local.build).toEqual(jasmine.any(Function));
+    });
+
+    it('should define phonegap.remote', function() {
         expect(phonegap.remote).toEqual(jasmine.any(Object));
+    });
+
+    it('should define phonegap.remote.build', function() {
+        expect(phonegap.remote.build).toEqual(jasmine.any(Function));
+    });
+
+    it('should define phonegap.remote.login', function() {
+        expect(phonegap.remote.login).toEqual(jasmine.any(Function));
+    });
+
+    it('should define phonegap.remote.logout', function() {
+        expect(phonegap.remote.logout).toEqual(jasmine.any(Function));
     });
 });
