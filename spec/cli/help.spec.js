e2d826f1059f7792b081972d2d26258c47640bc6
Lorin Beer <lorin.beer@gmail.com>
2014-10-23 13:38:16 -0700

    [#435] removed unnecessary wrapping 'defines', added additional tests for 'help help' command, #434

--- spec/cli/help.spec.js
+++ spec/cli/help.spec.js
@@ -10,7 +10,7 @@ var CLI = require('../../lib/cli'),
  * Specification: $ phonegap help
  */
 
-describe('phonegap help', function() {
+describe('phonegap', function() {
     beforeEach(function() {
         cli = new CLI();
         argv = ['node', '/usr/local/bin/phonegap'];
@@ -18,31 +18,40 @@ describe('phonegap help', function() {
         spyOn(process.stderr, 'write');
     });
 
-    describe('$ phonegap', function() {
-        it('should output the usage information', function() {
-            cli.argv(argv);
-            expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
-        });
+    it('should output the usage information when invoked with no arguments eg $ phonegap', function() {
+        cli.argv(argv);
+        expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
     });
 
-    describe('$ phonegap help', function() {
-        it('should output the usage information', function() {
-            cli.argv(argv.concat(['help']));
-            expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
-        });
+    it('should output the usage information when invoked with help argument eg $ phonegap help', function() {
+        cli.argv(argv.concat(['help']));
+        expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
     });
 
-    describe('$ phonegap --help', function() {
-        it('should output the usage information', function() {
-            cli.argv(argv.concat(['--help']));
-            expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
-        });
+    it('should output the usage information when invoked with help flag eg $ phonegap --help', function() {
+        cli.argv(argv.concat(['--help']));
+        expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
     });
 
-    describe('$ phonegap -h', function() {
-        it('should output the usage information', function() {
-            cli.argv(argv.concat(['-h']));
+    it('should output the usage information when invoked with abbreviated help flag $ phonegap -h', function() {
+        cli.argv(argv.concat(['-h']));
+        expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
+    });
+
+
+        it('should output the help usage information when invoked with help eg $ phonegap help help', function() {
+            cli.argv(argv.concat(['help', 'help']));
             expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
         });
-    });
+
+        it('should output the help usage information when invoked with help eg $ phonegap help --help', function() {
+            cli.argv(argv.concat(['help', '--help']));
+            expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
+        });
+ 
+        it('should output the help usage information when invoked with help eg $ phonegap help -h', function() {
+            cli.argv(argv.concat(['help', '-h']));
+            expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
+        });
+ 
 });
