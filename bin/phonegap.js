f224c2db5fdab8420db2d594248527d4ca1ef77b
Jesse MacFadyen <purplecabbage@gmail.com>
2016-05-12 01:27:41 -0700

    added tracking

--- bin/phonegap.js
+++ bin/phonegap.js
@@ -5,16 +5,42 @@
  */
 
 var CLI = require('../lib/cli');
+var cli = new CLI();
+var tracking = cli.tracking;
+var version = require('../package.json').version;
+
+if(tracking.statusUnknown()) {
+    tracking.prompt(function(){
+        // special case, if it is a 'phonegap tracking' command we don't want
+        // to run it because the prompt will have already done the work
+        if(process.argv[2] != "tracking"){
+            runPhoneGapCommand();
+        }
+    });
+}
+else {
+    runPhoneGapCommand();
+}
 
 /*!
  * Run the command-line client.
  */
+function runPhoneGapCommand() {
 
-var cli = new CLI().argv(process.argv, function(e) {
-    // if we receive an error, then exit with an error status
-    // if an exit code was attached to the error, then use it
-    // otherwise default to 1.
-    if (e) {
-        process.exit(e.exitCode || 1);
-    }
-});
+    cli.argv(process.argv, function(e) {
+
+        // tracking module will skip if it is not enabled
+
+        tracking.trackEvent("pg-cli " + version,
+                            process.argv[2],
+                            process.argv[3],
+                            e ? e.exitCode || 1 : 0 );
+
+        // if we receive an error, then exit with an error status
+        // if an exit code was attached to the error, then use it
+        // otherwise default to 1.
+        if (e) {
+            process.exit(e.exitCode || 1);
+        }
+    });
+}
