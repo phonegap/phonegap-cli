30420839ba383d58a93f0e555c90305d74beae69
Suraj Pindoria <suraj.pindoria@yahoo.com>
2016-06-08 13:49:14 -0700

    [#595] Ton of logging statments / spawn fixes

--- bin/phonegap.js
+++ bin/phonegap.js
@@ -11,6 +11,8 @@ var CLI = require('../lib/cli');
  */
 
 var cli = new CLI().argv(process.argv, function(e) {
+    console.log('*** phonegap bin phonegap.js');
+    // console.log(process.argv);
     // if we receive an error, then exit with an error status
     // if an exit code was attached to the error, then use it
     // otherwise default to 1.
