bee2702f4e1461d73d2f8d64972240db3256e88a
Suraj Pindoria <suraj.pindoria@yahoo.com>
2016-06-29 13:50:44 -0700

    [#595] Clean up desktop app template support and remove debug logging

--- bin/phonegap.js
+++ bin/phonegap.js
@@ -11,8 +11,6 @@ var CLI = require('../lib/cli');
  */
 
 var cli = new CLI().argv(process.argv, function(e) {
-    console.log('*** phonegap bin phonegap.js');
-    // console.log(process.argv);
     // if we receive an error, then exit with an error status
     // if an exit code was attached to the error, then use it
     // otherwise default to 1.
