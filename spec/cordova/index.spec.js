06b4bad86e3e78f4cbce04f116b34cba7b5d93b4
carynbear <caryn.tran@berkeley.edu>
2016-07-28 17:16:05 -0700

    working serve, need to fix tests and make serve's usage of cordova more transparent

--- spec/cordova/index.spec.js
+++ spec/cordova/index.spec.js
@@ -9,7 +9,7 @@ var cordova = require('../../lib/cordova'),
  * Specification: cordova
  */
 
-// ToDo: @carynbear phonegap is checking that cordova is a local dependency
+// ToDo: @carynbear clean this up; phonegap is checking that cordova is a local dependency
 describe('cordova.cordova', function() {
     xit('should be defined', function() {
         expect(cordova.cordova).toEqual(jasmine.any(Object));
@@ -17,14 +17,14 @@ describe('cordova.cordova', function() {
     });
 });
 
-// ToDo: @carynbear phonegap is checking that cordova is a local dependency
+// ToDo: @carynbear clean this up; phonegap is checking that cordova is a local dependency
 describe('cordova.lib', function() {
     xit('should be defined', function() {
         expect(cordova.lib).toEqual(jasmine.any(Object));
         expect(cordova.lib).toEqual(original);
     });
 
-    // ToDo: @carynbear phonegap is checking that cordova is a local dependency
+    // ToDo: @carynbear clean this up; phonegap is checking that cordova is a local dependency
     xit('should customize the binary name (binname)', function() {
         expect(cordova.lib.binname).toEqual('phonegap');
     });
