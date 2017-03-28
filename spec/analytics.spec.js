c7e2d0977a23c4001b862b69f3443ecf1dee0255
filmaj <maj.fil@gmail.com>
2017-03-28 13:25:09 -0700

    basic metrics.phonegap.com event tracking tests - closes #686.

--- spec/analytics.spec.js
+++ spec/analytics.spec.js
@@ -1,4 +1,6 @@
 var cli = require('../lib/cli');
+var analytics = require('../lib/cli/analytics');
+var request = require('request');
 
 function trigger_phonegap_cli() {
     require('../bin/phonegap');
@@ -17,5 +19,43 @@ describe('PhoneGap Analytics', function() {
         });
         trigger_phonegap_cli();
         expect(track).toHaveBeenCalledWith(jasmine.any(Array), fake_error);
+    });
+    describe('trackEvent', function() {
+        var post_spy;
+        beforeEach(function() {
+            spyOn(analytics, 'hasOptedOut').andReturn(false);
+            post_spy = spyOn(request, 'post');
+        });
+        it('should POST to metrics.phonegap.com on successfully-parsed commands', function() {
+            analytics.trackEvent(["platform", "list"]);
+            expect(post_spy).toHaveBeenCalled();
+        });
+        it('should attach command exit codes to event tracking', function() {
+            analytics.trackEvent(["platform", "explode"], {exitCode:21});
+            var dump = JSON.parse(post_spy.calls[0].args[0].form);
+            expect(dump._exitCode).toEqual(21);
+        });
+        it('should track top-level commands in the short_message event field', function() {
+            var cmds = ['serve', 'platform', 'plugins', 'version'];
+            cmds.forEach(function(cmd) {
+                analytics.trackEvent([cmd]);
+                var last_call = post_spy.calls[post_spy.calls.length - 1];
+                var dump = JSON.parse(last_call.args[0].form);
+                expect(dump.short_message).toEqual(cmd);
+            });
+        });
+        it('should track switches in the _switches event field', function() {
+            var cmd = ['serve', '--yourself', '--verbose'];
+            analytics.trackEvent(cmd);
+            var dump = JSON.parse(post_spy.calls[0].args[0].form);
+            expect(dump._flags).toContain('--yourself');
+            expect(dump._flags).toContain('--verbose');
+        });
+        it('should track parameters in the _params event field', function() {
+            var cmd = ['plugins', 'list'];
+            analytics.trackEvent(cmd);
+            var dump = JSON.parse(post_spy.calls[0].args[0].form);
+            expect(dump._params).toContain('list');
+        });
     });
 });
