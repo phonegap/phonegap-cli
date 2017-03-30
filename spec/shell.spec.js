382fb923033493446e57c50b3b5e682a9d2552a7
filmaj <maj.fil@gmail.com>
2017-03-30 16:23:34 -0700

    Dont actually shell out for the shell tests. Rename specs to "shell.spec.js", as there is both a cli.js and cli/ directory that "cli.spec.js" could refer to - hopefully this is slightly less confusing.

---
+++ spec/shell.spec.js
@@ -0,0 +1,62 @@
+var cli = require('../lib/cli');
+var analytics = require('../lib/cli/analytics');
+
+function trigger_phonegap_cli() {
+    require('../bin/phonegap');
+}
+
+describe('$ phonegap [options] commands', function() {
+    var orig_args;
+    beforeEach(function() {
+        // delete the phonegap cli entry module from require cache
+        // so we can exercise its logic in each test case
+        delete require.cache[require.resolve('../bin/phonegap')];
+        // ensure we dont prompt for turning analytics on
+        spyOn(cli.prototype.analytics, 'statusUnknown').andReturn(false);
+        // dont log analytics during tests - fake the cli out into thinking we opted out
+        spyOn(cli.prototype.analytics, 'hasOptedOut').andReturn(true);
+        orig_args = process.argv;
+    });
+    afterEach(function() {
+        process.argv = orig_args;
+    });
+
+    it('should support no arguments and post help', function() {
+        spyOn(console, 'log');
+        trigger_phonegap_cli();
+        expect(console.log.mostRecentCall.args[0]).toMatch('Usage:');
+    });
+
+    it('should support commands', function() {
+        process.argv = ['node', 'phonegap.js', 'version'];
+        spyOn(console, 'log');
+        trigger_phonegap_cli();
+        expect(console.log.mostRecentCall.args[0]).toMatch(/^\w+\.\w+\.\w+/);
+    });
+
+    it('should support options', function() {
+        process.argv = ['node', 'phonegap.js', '--version'];
+        spyOn(console, 'log');
+        trigger_phonegap_cli();
+        expect(console.log.mostRecentCall.args[0]).toMatch(/^\w+\.\w+\.\w+/);
+    });
+
+    it('should have exit code 0 on successful commands', function() {
+        process.argv = ['node', 'phonegap.js', '--version'];
+        spyOn(console, 'log');
+        trigger_phonegap_cli();
+        expect(process.exitCode).toEqual(0);
+    });
+
+    describe('on an error', function() {
+        it('should have non-zero exit code', function() {
+            process.argv = ['node', 'phonegap.js', 'cordova', 'noop'];
+            spyOn(cli.prototype, 'argv').andCallFake(function(args, cb) {
+                // have argv just blast back an error object to the callback to trigger error flow
+                cb({exitCode:1337});
+            });
+            trigger_phonegap_cli();
+            expect(process.exitCode).toEqual(1337);
+        });
+    });
+});
