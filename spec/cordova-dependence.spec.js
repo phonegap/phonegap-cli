fe6cb1190067a4bd87a78719b22b181c7656cd6d
filmaj <maj.fil@gmail.com>
2017-04-18 13:08:13 -0700

    cordova dependence tests no longer touch filesystem

--- spec/cordova-dependence.spec.js
+++ spec/cordova-dependence.spec.js
@@ -1,27 +1,45 @@
-//ToDo: here!
 var path = require('path'),
-    fs = require('fs'),
-    shell = require('shelljs'),
+    fs,
+    shell,
+    path,
+    fn,
     EventEmitter = require('events').EventEmitter,
     mockery = require('mockery'),
-    cordova_common = require('cordova-common'),
-    fn = require('../lib/phonegap/cordova-dependence').exec,
     processSpy = {
         stdout: new EventEmitter(),
         stderr: new EventEmitter()
     };
 
-var tests = path.resolve(__dirname, 'testcases');
+describe('Management of Cordova as an npm dependency', function() {
+    var config_parser_spy,
+        find_project_root_spy;
+    beforeEach(function() {
+        config_parser_spy = jasmine.createSpy('ConfigParserSpy');
+        find_project_root_spy = jasmine.createSpy('findProjectRootSpy');
+        mockery.enable({
+            warnOnUnregistered: false,
+            warnOnReplace: false,
+            useCleanCache: true
+        });
+        mockery.registerMock('cordova-common', {
+            ConfigParser: config_parser_spy,
+            CordovaCheck: {
+                findProjectRoot: find_project_root_spy
+            }
+        });
+        fn = require('../lib/phonegap/cordova-dependence').exec;
+        shell = require('shelljs');
+        fs = require('fs');
+        path = require('path');
+    });
+    afterEach(function() {
+        mockery.deregisterAll();
+        mockery.disable();
+    });
 
-
-//Not a package should error
-describe('phonegap cordova dependency tests', function() {
-    var projectDir;
-
-    iit ('should error if project is not a project', function(done) {
-        projectDir = path.resolve(tests, 'not_a_project');
-        spyOn(cordova_common.CordovaCheck, 'findProjectRoot').andReturn(false);
-        fn(projectDir)
+    it ('should error if project is not a valid cordova/phonegap project', function(done) {
+        find_project_root_spy.andReturn(false);
+        fn('my/sweet/project/path')
         .then(function() {
             expect('fail').toBe('thrown');
         }).fail(function(err) {
@@ -29,85 +47,132 @@ describe('phonegap cordova dependency tests', function() {
         }).fin(done);
     });
 
-    it('should error with no config.xml', function(done) {
-        projectDir = path.resolve(tests, 'no_config');
-        fn(projectDir)
-        .then(function() {
-            expect('fail').toBe('thrown');
-        }).fail(function(err) {
-            expect(err.message).toBe('"'+projectDir+'"' + ' does not point to a valid PhoneGap project');
-        }).fin(done);
-    });
-
-    describe ('no package.json', function() {
+    describe ('in a project with no package.json', function() {
         var emitter;
 
         beforeEach(function() {
+            find_project_root_spy.andReturn('/very/legit/project/directory');
+            spyOn(shell, 'cd');
             emitter = new EventEmitter();
-            spyOn(fs, 'writeFileSync');
-            spyOn(shell, 'which').andReturn(true);
-            spyOn(shell, 'exec').andCallFake(function(command, options, callback) {
-                callback(0, 'success');
-                return processSpy;
+            config_parser_spy.andCallFake(function(path) {
+                this.name = function() { return 'name'; };
+                this.packageName = function() { return 'package_name'; };
+                this.id = function() { return 'id'; };
+                this.version = function() { return 'version'; };
             });
+            spyOn(fs, 'writeFileSync');
+            spyOn(fs, 'existsSync').andReturn(false);
             spyOn(emitter, 'emit');
-            mockery.enable({ useCleanCache : true});
-            mockery.registerMock('../../spec/testcases/no_pkg_json/package.json', '../../spec/testcases/package.json');
-            mockery.warnOnUnregistered(false);
+            // this will define the package.json path that cordova-dependence will try to require
+            // need to mock this so we can control `require`
+            spyOn(path, 'relative').andReturn('my-package');
+            mockery.registerMock('my-package', {
+                dependencies: {
+                    cordova: "winnar"
+                }
+            });
         });
 
-        afterEach(function() {
-            mockery.resetCache();
-            mockery.deregisterAll();
-            mockery.disable();
-        });
-
-        it('should parse config.xml, create package.json, and use npm to install dependency', function(done) {
-            projectDir = path.resolve(tests, 'no_pkg_json');
-            fn(projectDir, emitter)
+        it('should parse config.xml and create a package.json based on config.xml values', function(done) {
+            fn('/some/random/directory', emitter)
             .then(function() {
-                expect(fs.writeFileSync).toHaveBeenCalledWith(projectDir+'/package.json', '{\n    "displayName": "NoPackageJson",\n    "name": "no.pkg.json",\n    "version": "2.0.0"\n}', 'utf8');
-                expect(shell.exec).toHaveBeenCalled();
+                expect(emitter.emit).toHaveBeenCalledWith('warn', 'No package.json was found for your project. Creating one from config.xml');
+                expect(fs.writeFileSync).toHaveBeenCalledWith('/very/legit/project/directory/package.json', '{\n    "displayName": "name",\n    "name": "package_name",\n    "version": "version"\n}', 'utf8');
             }).fail(function(err) {
+                console.log(err);
                 expect(err).not.toBeDefined();
             }).fin(done);
         });
+
+        describe('cordova installation', function() {
+            beforeEach(function() {
+                mockery.deregisterMock('my-package');
+                mockery.registerMock('my-package', {});
+            });
+            it('should fail if npm could not be found', function(done) {
+                spyOn(shell, 'which').andReturn(false);
+                fn('/some/random/directory', emitter)
+                .then(function() {
+                    expect('fail').toBe('thrown');
+                }).fail(function(err) {
+                    expect(err.message).toContain('"npm" command line tool is not installed');
+                }).fin(done);
+            });
+            describe('npm shell out', function() {
+                it('should return promise resolution on success', function(done) {
+                    spyOn(shell, 'which').andReturn(true);
+                    spyOn(shell, 'exec').andCallFake(function(cmd, opts, cb) {
+                        cb(0, 'stdout', 'stderr');
+                        return processSpy;
+                    });
+                    fn('/some/random/directory', emitter)
+                    .then(function() {
+                        expect(shell.exec.mostRecentCall.args[0]).toEqual('npm install cordova --save');
+                    }).fail(function(err) {
+                        console.log(err);
+                        expect(err).not.toBeDefined();
+                    }).fin(done);
+                });
+                it('should return promise rejection on failure', function(done) {
+                    spyOn(shell, 'which').andReturn(true);
+                    spyOn(shell, 'exec').andCallFake(function(cmd, opts, cb) {
+                        cb(1, 'stdout', 'stderr');
+                        return processSpy;
+                    });
+                    fn('/some/random/directory', emitter)
+                    .then(function() {
+                        expect('fail').toBe('thrown');
+                    }).fail(function(err) {
+                        expect(err.exitCode).toEqual(1);
+                        expect(err.message).toContain('Error from npm');
+                    }).fin(done);
+                });
+            });
+        });
     });
 
-    describe ('has package.json', function() {
+    describe('in a project with package.json', function() {
         var emitter;
         beforeEach(function() {
+            find_project_root_spy.andReturn('/very/legit/project/directory');
+            spyOn(shell, 'cd');
             emitter = new EventEmitter();
-            spyOn(fs, 'writeFileSync');
+            spyOn(fs, 'existsSync').andReturn(true);
             spyOn(shell, 'which').andReturn(true);
             spyOn(shell, 'exec').andCallFake(function(command, options, callback) {
                 callback(0, 'success');
                 return processSpy;
             });
             spyOn(emitter, 'emit');
+            // this will define the package.json path that cordova-dependence will try to require
+            // need to mock this so we can control `require`
+            spyOn(path, 'relative').andReturn('my-package');
         });
 
         it('should do nothing if cordova dependency exists', function(done) {
-            projectDir = path.resolve(tests, 'pkg_json_cordova');
-            fn(projectDir, emitter)
+            mockery.registerMock('my-package', {
+                dependencies: {
+                    cordova: "winnar"
+                }
+            });
+            fn('wtfjs', emitter)
             .then(function() {
-                expect(fs.writeFileSync).not.toHaveBeenCalled();
                 expect(shell.which).not.toHaveBeenCalled();
                 expect(shell.exec).not.toHaveBeenCalled();
             }).fail(function(err) {
+                console.log(err);
                 expect(err).not.toBeDefined();
             }).fin(done);
         });
 
-        it('should use npm to install dependency if not exists', function(done) {
-            // Find the latest version of Cordova published and install it
-            projectDir = path.resolve(tests, 'pkg_json_no_cordova');
-            fn(projectDir, emitter)
+        it('should use npm to install dependency if it does not exist in package.json', function(done) {
+            mockery.registerMock('my-package', {});
+            fn('i love rock and roll', emitter)
             .then(function() {
-                expect(fs.writeFileSync).not.toHaveBeenCalled();
                 expect(shell.which).toHaveBeenCalled();
-                expect(shell.exec).toHaveBeenCalled();
+                expect(shell.exec.mostRecentCall.args[0]).toEqual('npm install cordova --save');
             }).fail(function(err) {
+                console.log(err);
                 expect(err).not.toBeDefined();
             }).fin(done);
         });
